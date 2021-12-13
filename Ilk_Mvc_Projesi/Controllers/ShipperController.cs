98a31748d95c4e02c2786dcdc12c05caa6067f1f
onurysr <onurysr97@gmail.com>
2021-12-13 12:27:42 +0300

    Shipper Eklendi

---
+++ Ilk_Mvc_Projesi/Controllers/ShipperController.cs
@@ -0,0 +1,133 @@
+﻿using Ilk_Mvc_Projesi.Models;
+using Ilk_Mvc_Projesi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc_Project1.Controllers
+{
+    public class ShipperController : Controller
+    {
+        private readonly NorthwindContext _context;
+        public ShipperController(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = _context.Shippers.Include(x => x.Orders).ThenInclude(x => x.OrderDetails).OrderBy(x => x.CompanyName).ToList();
+            return View(model);
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            var shipper = _context.Shippers.Include(x => x.Orders).ThenInclude(x => x.OrderDetails).FirstOrDefault(x => x.ShipperId == id);
+            if (shipper == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shipper);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(ShipperViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(model));
+            }
+
+            var shipper = new Shipper()
+            {
+                CompanyName = model.CompanyName,
+                Phone = model.Phone
+            };
+            _context.Shippers.Add(shipper);
+            try
+            {
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Detail), new { id = shipper.ShipperId });
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError(string.Empty, $"{model.CompanyName} eklenirken Hata oluştu.");
+                return View(model);
+            }
+        }
+
+        public IActionResult Delete(int? shipperId)
+        {
+            var silinecek = _context.Shippers.FirstOrDefault(x => x.ShipperId == shipperId);
+            if (silinecek == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            try
+            {
+                _context.Shippers.Remove(silinecek);
+                _context.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction(nameof(Index));
+            }
+            TempData["silinen_shipper"] = silinecek.CompanyName;
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Update(int? id)
+        {
+            var shipper = _context.Shippers.FirstOrDefault(x => x.ShipperId == id);
+            if (shipper == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            var model = new ShipperViewModel()
+            {
+                ShipperId = shipper.ShipperId,
+                CompanyName = shipper.CompanyName,
+                Phone = shipper.Phone
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(ShipperViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var shipper = _context.Shippers.FirstOrDefault(x => x.ShipperId == model.ShipperId);
+            try
+            {
+                shipper.CompanyName = model.CompanyName;
+                shipper.Phone = model.Phone;
+                _context.Shippers.Update(shipper);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Detail), new { id = shipper.ShipperId });
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError(string.Empty, $"{model.CompanyName} eklenirken Hata oluştu.");
+                return View(model);
+            }
+        }
+    }
+}
+
---
+++ Ilk_Mvc_Projesi/ViewModels/ShipperViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ilk_Mvc_Projesi.ViewModels
+{
+    public class ShipperViewModel
+    {
+
+        public int ShipperId { get; set; }
+        [StringLength(40, ErrorMessage = "Şirket adı 40 karakterden fazla olamaz.")]
+        [Required(ErrorMessage = "Bu Alan Boş Olamaz")]
+        public string CompanyName { get; set; }
+        [StringLength(15, ErrorMessage = "Telefon numarası 15 karakterden fazla olamaz.")]
+        public string Phone { get; set; }
+    }
+}

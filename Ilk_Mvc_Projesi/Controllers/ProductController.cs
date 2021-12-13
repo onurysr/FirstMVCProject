727e194a4962c5f88def4a14c379cb0272b3416a
onurysr <onurysr97@gmail.com>
2021-12-13 12:19:20 +0300

    product Eklendi

--- Ilk_Mvc_Projesi/Controllers/ProductController.cs
+++ Ilk_Mvc_Projesi/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Ilk_Mvc_Projesi.Models;
+using Ilk_Mvc_Projesi.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -6,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Ilk_Mvc_Projesi.Controllers
+namespace Mvc_Project1.Controllers
 {
     public class ProductController : Controller
     {
@@ -18,19 +19,124 @@ namespace Ilk_Mvc_Projesi.Controllers
         }
         public IActionResult Index()
         {
-            var model = _context.Products.Include(x=> x.Category).OrderBy(x => x.ProductId).ToList();
+            var model = _context.Products.Include(x => x.OrderDetails)
+                .OrderBy(x => x.ProductName).ToList();
             return View(model);
         }
 
         public IActionResult Detail(int? id)
         {
-            var Product = _context.Products.FirstOrDefault(x => x.ProductId == id);
-            if (Product == null)
+            var model = _context.Products.Include(x => x.OrderDetails).FirstOrDefault(x => x.ProductId == id);
+
+            if (model == null)
             {
                 return RedirectToAction(nameof(Index));
             }
-            return View(Product);
+            return View(model);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var product = new Product()
+            {
+                ProductName = model.ProductName,
+                UnitPrice = model.UnitPrice,
+                UnitsInStock = (short)model.UnitsInStock
+
+            };
+            _context.Products.Add(product);
+            try
+            {
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Detail), new { id = product.ProductId });
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError(string.Empty, $"{model.ProductName} eklenirken bir hata oluştu. Tekrar Deneyin");
+                ModelState.AddModelError(string.Empty, $"{model.UnitPrice} eklenirken bir hata oluştu. Tekrar Deneyin");
+                ModelState.AddModelError(string.Empty, $"{model.UnitsInStock} eklenirken bir hata oluştu. Tekrar Deneyin");
+                return View(model);
+            }
+        }
+
+        public IActionResult Delete(int? productId)
+        {
+            var silinecek = _context.Products.FirstOrDefault(x => x.ProductId == productId);
+            try
+            {
+                _context.Products.Remove(silinecek);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction(nameof(Index));
+            }
+            TempData["silinen_urun"] = silinecek.ProductName;
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Update(int? id)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.ProductId == id);
+            if (product == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            var model = new ProductViewModel()
+            {
+                ProductId = product.ProductId,
+                ProductName = product.ProductName,
+                UnitPrice = (decimal)product.UnitPrice,
+                UnitsInStock = (int)product.UnitsInStock
+
+
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(ProductViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var product = _context.Products.FirstOrDefault(x => x.ProductId == model.ProductId);
+            try
+            {
+                product.ProductName = model.ProductName;
+                product.UnitPrice = model.UnitPrice;
+                product.UnitsInStock = (short)model.UnitsInStock;
+
+                _context.Products.Update(product);
+
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Detail), new { id = product.ProductId });
+            }
+            catch (Exception)
+            {
+
+                ModelState.AddModelError(string.Empty, $"{model.ProductName} eklenirken bir hata oluştu. Tekrar Deneyin");
+                ModelState.AddModelError(string.Empty, $"{model.UnitPrice} eklenirken bir hata oluştu. Tekrar Deneyin");
+                ModelState.AddModelError(string.Empty, $"{model.UnitsInStock} eklenirken bir hata oluştu. Tekrar Deneyin");
+                return View(model);
+            }
+        }
     }
 }
---
+++ Ilk_Mvc_Projesi/ViewModels/ProductViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ilk_Mvc_Projesi.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Required(ErrorMessage = "Ürün Adı Boş olamaz")]
+        [Display(Name = "Ürün Adı")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "Fiyat Boş olamaz")]
+        [Display(Name = "Fiyat")]
+        public decimal UnitPrice { get; set; }
+        [Required(ErrorMessage = "Stok Sayısı Boş olamaz")]
+        [Display(Name = "Stok Sayısı")]
+        public int UnitsInStock { get; set; }
+
+        public int ProductId { get; set; }
+
+    }
+}

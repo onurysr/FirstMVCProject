d2aa53bfc5314f7ec55341757070c2ff22384a52
onurysr <onurysr97@gmail.com>
2021-12-18 15:01:29 +0300

    supplier bitti

--- Ilk_Mvc_Projesi/Controllers/SupplierController.cs
+++ Ilk_Mvc_Projesi/Controllers/SupplierController.cs
@@ -50,5 +50,112 @@ namespace Ilk_Mvc_Projesi.Controllers
             return View(model);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(SupplierViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var supplier = new Supplier()
+            {
+                CompanyName = model.CompanyName,
+                City = model.City,
+                Country = model.Country,
+                ContactName = model.ContactName
+
+            };
+
+            try
+            {
+                _context.Suppliers.Add(supplier);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Detail), new { supplier.SupplierId });
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View(model);
+            }
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var silinecekSupp = _context.Suppliers.FirstOrDefault(x => x.SupplierId == id);
+            if (silinecekSupp == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            try
+            {
+                _context.Suppliers.Remove(silinecekSupp);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        public IActionResult Update(int? id)
+        {
+            var data = _context.Suppliers.FirstOrDefault(x => x.SupplierId == id);
+            if (data == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            var model = new SupplierViewModel()
+            {
+                SupplierId = data.SupplierId,
+                City = data.City,
+                CompanyName = data.CompanyName,
+                ContactName = data.ContactName,
+                Country = data.Country
+            };
+
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public IActionResult Update(SupplierViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var supplier = _context.Suppliers.FirstOrDefault(x => x.SupplierId == model.SupplierId);
+            if (supplier == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            supplier.CompanyName = model.CompanyName;
+            supplier.ContactName = model.ContactName;
+            supplier.Country = model.Country;
+            supplier.City = model.City;
+            try
+            {
+                _context.Suppliers.Update(supplier);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Detail), new { supplier.SupplierId });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View(model);
+            }
+        }
+
     }
 }

a5b1445d9dc53ec5886ecbedd1dcfb2a98768d98
onurysr <onurysr97@gmail.com>
2021-12-18 14:03:58 +0300

    SuppilerContoller Eklendi

---
+++ Ilk_Mvc_Projesi/Controllers/SupplierController.cs
@@ -0,0 +1,54 @@
+﻿using Ilk_Mvc_Projesi.Models;
+using Ilk_Mvc_Projesi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ilk_Mvc_Projesi.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly NorthwindContext _context;
+        public SupplierController(NorthwindContext context)
+        {
+            _context = context;
+        }
+        private int _pagesize = 8;
+        public IActionResult Index(int? page = 1)
+        {
+            var model = _context.Suppliers
+                .OrderBy(x => x.CompanyName)
+                .Skip((page.GetValueOrDefault() - 1) * _pagesize)
+                .Take(_pagesize)
+                .ToList();
+
+            ViewBag.Page = page.GetValueOrDefault(1);
+            ViewBag.Limit = (int)Math.Ceiling(_context.Suppliers.Count() / (double)_pagesize);
+            return View(model);
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            var data = _context.Suppliers.FirstOrDefault(x => x.SupplierId == id);
+            if (data == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            var model = new SupplierViewModel()
+            {
+                SupplierId = data.SupplierId,
+                City = data.City,
+                CompanyName = data.CompanyName,
+                ContactName = data.ContactName,
+                ContactTitle = data.ContactTitle,
+                Country = data.Country
+            };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Ilk_Mvc_Projesi/ViewModels/SupplierViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ilk_Mvc_Projesi.ViewModels
+{
+    public class SupplierViewModel
+    {
+        public int SupplierId { get; set; }
+        [Required(ErrorMessage ="Bu alan boş olamaz")]
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+    }
+}

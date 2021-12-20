add0be06eb6c9bda9d95e0b3dcf226f0f7cbae13
onurysr <onurysr97@gmail.com>
2021-12-20 18:35:49 +0300

    Product Api Oluşturuldu

---
+++ Ilk_Mvc_Projesi/Controllers/Apis/ProductApiController.cs
@@ -0,0 +1,51 @@
+﻿using Ilk_Mvc_Projesi.Models;
+using Ilk_Mvc_Projesi.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ilk_Mvc_Projesi.Controllers.Apis
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+        public ProductApiController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductViewModel model)
+        {
+            var product = new Product()
+            {
+                CategoryId = model.CategoryId,
+                ProductName = model.ProductName,
+                UnitPrice = model.UnitPrice
+            };
+
+            try
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return Ok(new
+                {
+                    Message = "Ürün Ekleme İşlemi Başarılı",
+                    Model = product
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest($"Bir Hata oluştu:{ex.Message}");
+            }
+        }
+    }
+}

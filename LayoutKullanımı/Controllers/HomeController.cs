4ce94cc400d2be83a9270c50c5209af1e6620152
onurysr <onurysr97@gmail.com>
2021-12-21 18:00:08 +0300

    HomeController ve Index eklendi

---
+++ LayoutKullanımı/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using LayoutKullanımı.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LayoutKullanımı.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new List<Urun>()
+            {
+                new Urun() { Id = 1, Ad = "Kalem",Fiyat = 5},
+                new Urun()
+                {
+                    Id = 2,
+                    Ad = "Kitap",
+                    Fiyat = 10
+                }
+            };
+            return View(model);
+        }
+    }
+}

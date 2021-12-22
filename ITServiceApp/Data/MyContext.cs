712751ed2c7ecb4e86d58f96330dc8860b6e8270
onurysr <onurysr97@gmail.com>
2021-12-22 20:48:21 +0300

    StartUp Identity options Eklendi

---
+++ ITServiceApp/Data/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using ITServiceApp.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITServiceApp.Data
+{
+    public class MyContext:IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public MyContext(DbContextOptions options)
+            :base(options)
+        {
+        }
+    }
+}
--- ITServiceApp/Startup.cs
+++ ITServiceApp/Startup.cs
@@ -1,6 +1,10 @@
+using ITServiceApp.Data;
+using ITServiceApp.Models.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +16,45 @@ namespace ITServiceApp
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MyContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlConnection"));
+            });
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+             {
+                 options.Password.RequireUppercase = false;
+                 options.Password.RequireLowercase = true;
+                 options.Password.RequireDigit = true;
+                 options.Password.RequiredLength = 5;
+
+                 //lockout settings
+                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                 options.Lockout.MaxFailedAccessAttempts = 3;
+                 options.Lockout.AllowedForNewUsers = false;
+
+                 //User settings
+                 options.User.AllowedUserNameCharacters = "abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ-._@+";
+                 options.User.RequireUniqueEmail = true;
+             });
+            services.ConfigureApplicationCookie(options =>
+            {
+                //cookie settings
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccesDenied";
+                options.SlidingExpiration = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

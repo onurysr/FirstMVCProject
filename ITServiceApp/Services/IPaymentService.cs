68ace4bd45d0356e9d00407d1f7ef08020c0924b
onurysr <onurysr97@gmail.com>
2022-01-17 20:59:04 +0300

    PaymentService Eklendi

--- ITServiceApp/Services/IPaymentService.cs
+++ ITServiceApp/Services/IPaymentService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ITServiceApp.Models.Payment;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,6 @@ namespace ITServiceApp.Services
     public interface IPaymentService
     {
         public void CheckInstallments(string binNumber, decimal price);
-        public void Pay();
+        public PaymentResponseModel Pay(PaymentModel model);
     }
 }
---
+++ ITServiceApp/Services/IyzicoPaymentService.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using ITServiceApp.Models.Payment;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITServiceApp.Services
+{
+    public class IyzicoPaymentService : IPaymentService
+    {
+        private readonly IConfiguration _configuration; //appsetinge erişmek için
+        private readonly IyzicoPaymentOptions _options;
+        private readonly IMapper _mapper;
+
+        public IyzicoPaymentService(IConfiguration configuration,IyzicoPaymentOptions options, IMapper mapper)
+        {
+            _configuration = configuration;
+            _options = options;
+            _mapper = mapper;
+
+            var section = _configuration.GetSection(IyzicoPaymentOptions.Key);
+            _options = new IyzicoPaymentOptions()
+            {
+                ApiKey = section["ApiKey"],
+                SecretKey = section["SecretKey"],
+                BaseUrl = section["BaseUrl"],
+                ThreedsCallbackUrl = section["ThreedsCallbackUrl"]
+            };
+        }
+        public void CheckInstallments(string binNumber, decimal price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PaymentResponseModel Pay(PaymentModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
